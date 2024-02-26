05ecce091eb9d8ffe98793ad6781a8f0ce38cc6b
adilavirk <adilavirk0@gmail.com>
2024-02-26 11:10:29 +0500

    fixed some issues

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,3 +1,4 @@
+"use client"
 import React, { useEffect, useState } from 'react';
 import { auth } from '../(firebase)/firebase';
 import { createUserWithEmailAndPassword, updateProfile, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
